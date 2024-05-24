5cfa618cfe532ca7d8222c9daf43fb57b924aa96
bmsbrothers <nanangbmsbrothers@gmail.com>
2024-05-25 00:30:55 +0700

    Updates

---
+++ precache-manifest.530dca8140a1e4f8aeae84e7dd92f819.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aff746a2382349d7005bee6b0155e1f",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "6c8b92bc3d25a81f3965",
+    "url": "/website/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "87fed78ce46543d67955",
+    "url": "/website/static/css/main.201fbec9.chunk.css"
+  },
+  {
+    "revision": "6c8b92bc3d25a81f3965",
+    "url": "/website/static/js/2.d84b9ed7.chunk.js"
+  },
+  {
+    "revision": "97b5938ef990e58844f0cff8514cc372",
+    "url": "/website/static/js/2.d84b9ed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87fed78ce46543d67955",
+    "url": "/website/static/js/main.13776742.chunk.js"
+  },
+  {
+    "revision": "79023f35d1a5aef077b9",
+    "url": "/website/static/js/runtime-main.b71e1fc7.js"
+  }
+]);
